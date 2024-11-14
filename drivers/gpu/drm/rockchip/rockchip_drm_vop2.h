bce2948c767ae2efabf058728c06d61bacebda86
Sebastian Reichel <sebastian.reichel@collabora.com>
2024-11-14 17:36:18 +0100

    drm/rockchip: vop2: Add core reset support
    A previous from Detlev Casanova adds reset handling for the
    video ports. This also resets the AHB and AXI interface when
    the system binds the VOP2 controller.
    
    This fixes issues when the bootloader (or a previously running
    kernel when using kexec) left the VOP2 initialized to some degree.
    
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.h
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.h
@@ -8,6 +8,7 @@
 #define _ROCKCHIP_DRM_VOP2_H
 
 #include <linux/regmap.h>
+#include <linux/reset.h>
 #include <drm/drm_modes.h>
 #include <dt-bindings/soc/rockchip,vop2.h>
 #include "rockchip_drm_drv.h"
@@ -164,6 +165,12 @@ enum vop2_win_regs {
 	VOP2_WIN_DLY_NUM,
 
 	VOP2_WIN_MAX_REG,
+};
+
+enum {
+	RST_ACLK,
+	RST_HCLK,
+	RST_VOP2_MAX
 };
 
 struct vop2_regs_dump {
@@ -331,6 +338,7 @@ struct vop2 {
 	struct clk *pclk;
 	struct clk *pll_hdmiphy0;
 	struct clk *pll_hdmiphy1;
+	struct reset_control_bulk_data resets[RST_VOP2_MAX];
 
 	/* optional internal rgb encoder */
 	struct rockchip_rgb *rgb;
