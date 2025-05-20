7bbb659b14a82237458ede658b02e4559e75a89f
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2025-05-20 12:26:58 +0200

    accel/rocket: Add a new driver for Rockchip's NPU
    This initial version supports the NPU as shipped in the RK3588 SoC and
    described in the first part of its TRM, in Chapter 36.
    
    This NPU contains 3 independent cores that the driver can submit jobs
    to.
    
    This commit adds just hardware initialization and power management.
    
    v2:
    - Split cores and IOMMUs as independent devices (Sebastian Reichel)
    - Add some documentation (Jeffrey Hugo)
    - Be more explicit in the Kconfig documentation (Jeffrey Hugo)
    - Remove resets, as these haven't been found useful so far (Zenghui Yu)
    - Repack structs (Jeffrey Hugo)
    - Use DEFINE_DRM_ACCEL_FOPS (Jeffrey Hugo)
    - Use devm_drm_dev_alloc (Jeffrey Hugo)
    - Use probe log helper (Jeffrey Hugo)
    - Introduce UABI header in a later patch (Jeffrey Hugo)
    
    v3:
    - Adapt to a split of the register block in the DT bindings (Nicolas
      Frattaroli)
    - Move registers header to its own commit (Thomas Zimmermann)
    - Misc. cleanups (Thomas Zimmermann and Jeff Hugo)
    - Make use of GPL-2.0-only for the copyright notice (Jeff Hugo)
    - PM improvements (Nicolas Frattaroli)
    
    v4:
    - Use bulk clk API (Krzysztof Kozlowski)
    
    Signed-off-by: Tomeu Vizoso <tomeu@tomeuvizoso.net>

---
+++ drivers/accel/rocket/rocket_device.c
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/* Copyright 2024-2025 Tomeu Vizoso <tomeu@tomeuvizoso.net> */
+
+#include <linux/array_size.h>
+#include <linux/clk.h>
+#include <linux/dev_printk.h>
+
+#include "rocket_device.h"
+
+int rocket_device_init(struct rocket_device *rdev)
+{
+	struct device *dev = rdev->cores[0].dev;
+	int err;
+
+	err = devm_clk_bulk_get(dev, ARRAY_SIZE(rdev->clks), rdev->clks);
+	if (err)
+		return dev_err_probe(dev, err, "failed to get device clocks\n");
+
+	/* Initialize core 0 (top) */
+	err = rocket_core_init(&rdev->cores[0]);
+	if (err)
+		return err;
+
+	return 0;
+}
+
+void rocket_device_fini(struct rocket_device *rdev)
+{
+	rocket_core_fini(&rdev->cores[0]);
+}
