5f04e3120c4044ab3269b42a13258a9fda0462e7
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2025-06-06 08:28:24 +0200

    accel/rocket: Add job submission IOCTL
    Using the DRM GPU scheduler infrastructure, with a scheduler for each
    core.
    
    Userspace can decide for a series of tasks to be executed sequentially
    in the same core, so SRAM locality can be taken advantage of.
    
    The job submission code was initially based on Panfrost.
    
    v2:
    - Remove hardcoded number of cores
    - Misc. style fixes (Jeffrey Hugo)
    - Repack IOCTL struct (Jeffrey Hugo)
    
    v3:
    - Adapt to a split of the register block in the DT bindings (Nicolas
      Frattaroli)
    - Make use of GPL-2.0-only for the copyright notice (Jeff Hugo)
    - Use drm_* logging functions (Thomas Zimmermann)
    - Rename reg i/o macros (Thomas Zimmermann)
    - Add padding to ioctls and check for zero (Jeff Hugo)
    - Improve error handling (Nicolas Frattaroli)
    
    v6:
    - Use mutexes guard (Markus Elfring)
    - Use u64_to_user_ptr (Jeff Hugo)
    - Drop rocket_fence (Rob Herring)
    
    v7:
    - Assign its own IOMMU domain to each client, for isolation (Daniel
      Stone and Robin Murphy)
    
    Signed-off-by: Tomeu Vizoso <tomeu@tomeuvizoso.net>

--- drivers/accel/rocket/rocket_drv.h
+++ drivers/accel/rocket/rocket_drv.h
@@ -4,12 +4,16 @@
 #ifndef __ROCKET_DRV_H__
 #define __ROCKET_DRV_H__
 
+#include <drm/gpu_scheduler.h>
+
 #include "rocket_device.h"
 
 struct rocket_file_priv {
 	struct rocket_device *rdev;
 
 	struct iommu_domain *domain;
+
+	struct drm_sched_entity sched_entity;
 };
 
 #endif
