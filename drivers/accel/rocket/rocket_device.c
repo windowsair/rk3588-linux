df115229a6d568661dd98659453ea56547dc45e3
Tomeu Vizoso <tomeu@tomeuvizoso.net>
2025-06-06 08:28:23 +0200

    accel/rocket: Add IOCTL for BO creation
    This uses the SHMEM DRM helpers and we map right away to the CPU and NPU
    sides, as all buffers are expected to be accessed from both.
    
    v2:
    - Sync the IOMMUs for the other cores when mapping and unmapping.
    
    v3:
    - Make use of GPL-2.0-only for the copyright notice (Jeff Hugo)
    
    v6:
    - Use mutexes guard (Markus Elfring)
    
    v7:
    - Assign its own IOMMU domain to each client, for isolation (Daniel
      Stone and Robin Murphy)
    
    Reviewed-by: Jeffrey Hugo <quic_jhugo@quicinc.com>
    Signed-off-by: Tomeu Vizoso <tomeu@tomeuvizoso.net>

--- drivers/accel/rocket/rocket_device.c
+++ drivers/accel/rocket/rocket_device.c
@@ -4,6 +4,7 @@
 #include <linux/array_size.h>
 #include <linux/clk.h>
 #include <linux/dev_printk.h>
+#include <linux/mutex.h>
 
 #include "rocket_device.h"
 
@@ -16,10 +17,13 @@ int rocket_device_init(struct rocket_device *rdev)
 	if (err)
 		return err;
 
+	mutex_init(&rdev->iommu_lock);
+
 	return 0;
 }
 
 void rocket_device_fini(struct rocket_device *rdev)
 {
+	mutex_destroy(&rdev->iommu_lock);
 	rocket_core_fini(&rdev->cores[0]);
 }
